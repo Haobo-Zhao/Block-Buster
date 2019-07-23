5af03173b4ffe57ca3b1dbde68a4deb59e35dfae
Haobo-Zhao <haobo4608@gmail.com>
2019-07-23 12:15:18 -0400

    initialize the elements inside scene after entering canvas

--- season2/ep7 - editor/scene/editor/scene_editor.js
+++ season2/ep7 - editor/scene/editor/scene_editor.js
@@ -6,23 +6,26 @@ class SceneEditor extends Scene {
     }
 
     init() {
-        this.level = []
     }
 
     __bindingEvents() {
         this.game.canvas.addEventListener('mouseenter', (event) => {
-            this.b = Brick(this.game, 0, 0)
+            this.level = []
+            this.b = Brick(this.game)
         })
 
         this.game.canvas.addEventListener('mousemove', (event) => {
+            if (!this.b) {
+                return
+            }
             const mouseX = event.offsetX
             const mouseY = event.offsetY
             // 一格一格的排列效果
             const newX = (mouseX - this.b.w / 2) - (mouseX - this.b.w / 2) % 25
             const newY = (mouseY - this.b.h / 2) - (mouseY - this.b.h / 2) % 20
             const newPos = {
-                x: clamp(newX, 0, 400 - this.b.w),
-                y: clamp(newY, 0, 300 - this.b.h),
+                x: clamp(newX, 0, this.game.canvas.width - this.b.w),
+                y: clamp(newY, 0, this.game.canvas.height - this.b.h),
             }
             Object.assign(this.b, newPos)
         })
