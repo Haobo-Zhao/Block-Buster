d4b84991ca5976df7dbc86ed1e9607308469849b
Haobo-Zhao <haobo4608@gmail.com>
2019-07-23 12:11:24 -0400

    render the editing brick after entering canvas

--- season2/ep7 - editor/scene/editor/scene_editor.js
+++ season2/ep7 - editor/scene/editor/scene_editor.js
@@ -7,26 +7,24 @@ class SceneEditor extends Scene {
 
     init() {
         this.level = []
-        this.b = Brick(this.game, 0, 0)
-        window.image = this.b.image
-        window.image.style.opacity = 0.8
-        this.b.image.style.opacity = "0.4"
-        log(this.b.image)
-        document.getElementsByTagName('body')[0].appendChild(window.image)
     }
 
     __bindingEvents() {
+        this.game.canvas.addEventListener('mouseenter', (event) => {
+            this.b = Brick(this.game, 0, 0)
+        })
+
         this.game.canvas.addEventListener('mousemove', (event) => {
-            const x = event.offsetX
-            const y = event.offsetY
+            const mouseX = event.offsetX
+            const mouseY = event.offsetY
             // 一格一格的排列效果
-            const newX = (x - this.b.w / 2) - (x - this.b.w / 2) % 25
-            const newY = (y - this.b.h / 2) - (y - this.b.h / 2) % 20
-            const pos = {
+            const newX = (mouseX - this.b.w / 2) - (mouseX - this.b.w / 2) % 25
+            const newY = (mouseY - this.b.h / 2) - (mouseY - this.b.h / 2) % 20
+            const newPos = {
                 x: clamp(newX, 0, 400 - this.b.w),
                 y: clamp(newY, 0, 300 - this.b.h),
             }
-            Object.assign(this.b, pos)
+            Object.assign(this.b, newPos)
         })
     }
 
@@ -39,7 +37,7 @@ class SceneEditor extends Scene {
 
         this.game.context.save()
         this.game.context.globalAlpha = 0.4;
-        this.game.drawElement(this.b)
+        this.b && this.game.drawElement(this.b)
         this.game.context.restore()
     }
 
